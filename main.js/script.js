8a0d10d2547bc3a930795bf8e9c07bade6f92d71
Utkarsh736 <utkarshtomar736@gmail.com>
2022-02-03 16:14:04 +0530

    Operate function added

--- main.js/script.js
+++ main.js/script.js
@@ -1,4 +1,4 @@
-let result;
+let result = 0;
 
 function add(num1, num2){
     result = num1 + num2;
@@ -16,11 +16,28 @@ function multiply(num1, num2){
 };
 
 function divide(num1, num2){
-    if (num2==0){
-        return 'Undefined';
-    }
-    else{
-        result = num1/num2;
-        return result;
+    result = num1/num2;
+    return result;
+};
+
+
+function operate(operator, num1, num2){
+    switch(operator){
+
+        case('add'):
+            add(num1, num2);
+            break;
+
+        case('subtract'):
+            subtract(num1, num2);
+            break;
+
+        case('multiply'):
+            multiply(num1, num2);
+            break;
+
+        case('divide'):
+            divide(num1, num2);
+            break;
     };
 };
