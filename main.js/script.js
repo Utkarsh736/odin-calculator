376fcec817a179350b66c2f54b873466caa8b610
Utkarsh736 <utkarshtomar736@gmail.com>
2022-02-09 20:24:59 +0530

    Store function added

--- main.js/script.js
+++ main.js/script.js
@@ -5,10 +5,14 @@ const result_div = document.querySelectorAll('.display');
 const clr_btn = document.querySelector('#clr');
 const dlt_btn = document.querySelector('#dlt');
 const eql_btn = document.querySelector('#eql');
+const DEFAULT_VALUE = 0;
 
-let result = 0;
+let result = DEFAULT_VALUE;
 let value = '';
-
+let prev_val;
+let cur_val;
+let operator;
+let flag = 0;
 
 function add(num1, num2){
     result = num1 + num2;
@@ -52,6 +56,28 @@ function operate(operator, num1, num2){
     };
 };
 
+
+function store(val, operation){
+    if(flag==1){
+        cur_val = val;
+        // prev_val = cur_val;
+        result = operate(operation, prev_val, cur_val);
+        console.log(`result: ${result}`);
+        // displayResult(result);
+        console.log(`cur: ${cur_val}`)
+        console.log(`prev: ${prev_val}`);
+        console.log(`opert: ${operation}`);
+        flag = 0;
+    }
+    else if(flag==0){
+        prev_val = val;
+        console.log(`cur: ${cur_val}`)
+        console.log(`prev: ${prev_val}`);
+        console.log(`opert: ${operation}`);
+        flag = 1;
+    };
+};
+
 function displayResult(result){
     result_div[0].innerHTML = `${result}`;
 };
@@ -65,14 +91,30 @@ function main(){
     });
     
     clr_btn.addEventListener('click', ()=>{
+        value = '';
         displayResult(0);
     });
+
+    eql_btn.addEventListener('click', ()=>{
+        console.log(operator);
+        console.log(prev_val);
+        console.log(cur_val);
+        // operate(operator, prev_val, cur_val);
+    });
+
 };
 
-// function clear(){};
 
+operations[0].childNodes.forEach(operation =>{
+    operation.addEventListener('click', ()=>{
+        operator = operation.id;
+        store(value, operator);
+        value = '';
+        displayResult(DEFAULT_VALUE);
+    })
+})
 
 window.onload = function(){
     main();
-    displayResult(value);
+    displayResult(result);
 };
